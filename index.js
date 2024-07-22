f0be5ac8f6a2f0e9337a3e2a41f19c910ee31a0e
Waqar Shaikh <waqarshaikh789@gmail.com>
2024-07-22 12:27:35 +0530

    cors fixed

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 
 // Middleware
 app.use(bodyParser.json());
-app.use(cors('*'));
+app.use(cors());
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
